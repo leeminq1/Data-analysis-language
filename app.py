141e1caf3dc3a55f4851cef4d33d2cec1fc31973
leeminq1 <70461938+leeminq1@users.noreply.github.com>
2024-05-29 19:36:49 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -12,15 +12,16 @@ from pathlib import Path
 
 def pages():
     main_page = 'app.py'
-    salary_page = "pages/age_salary.py"
-    another_page = "pages/another.py"
-
+    seoul_main = "pages/seoul_main.py"
+    jongro_food_corr = "pages/jongro_food_corr.py"
+    jonro_food_graph = "pages/jonro_food_graph.py"
 
     show_pages(pages=
         [
-            Page(main_page, "Comment", icon="💛"),
-            Page(salary_page, "연령별 임금 및 근로시간", icon="🟩"),
-            Page(another_page, "Another Page", icon="🟥")
+            Page(main_page, "상권분석 개요", icon="🔶"),
+            Page(seoul_main, "서울시 자치구내 상권구분", icon="🔷"),
+            Page(jongro_food_corr, "종로구 요식업 - 변수/상관관계", icon="🟨"),
+            Page(jonro_food_graph, "종로구 요식업 - 그래프 시각화", icon="🟥"),
         ]
     )
 
@@ -52,28 +53,37 @@ def fontRegistered():
 
 
 def main_text():
-    st.header("통계청 데이터 기반 시각화")
-    st.markdown("**자료갱신일:** 2023-12-26")
-    st.text('Streamlit 시각화 공부를 위해 통계청 데이터 기반 시각화 r1')
+    st.header("데이터분석언어 6조 - 상권분석")
+    st.markdown("**데이터 갱신일:** 2024-05-26")
+    st.subheader('상권분석 가설')
+
+    st.markdown("""
+    **상권분석을 위한 데이터 분석**
+
+    - 가설 1 : 유동인구와 매출 간의 상관관계
+    - 가설 2 : 직장인 수, 거주자 수와 매출과의 상관관계
+    - 가설 3 : 주변 업소 수(개/폐점, 주변 업체 수)와 매출 간의 상관관계
+    - 가설 4 : 코로나19와 매출 간의 상관관계
+    """)
 
 
-def data_index_btn():
-    data_list = ['연령별 임금 및 근로시간', '연령별 임금 및 근로시간2']
-    clicked_buttons = [False] * len(data_list)  # 각 버튼의 클릭 여부를 저장하는 리스트
-
-    for i, data in enumerate(data_list):
-        clicked_buttons[i] = st.button(f'{data}')
-
-    # 각 버튼의 클릭 여부를 확인하여 이벤트 처리
-    for i, data in enumerate(data_list):
-        if clicked_buttons[i]:
-            # 페이지 이동을 위한 URL 설정
-            if data == '연령별 임금 및 근로시간':
-                page_url = 'pages/age_salary.py'
-                st.switch_page(page_url) ### page 이동 ###
-
-            elif data == '연령별 임금 및 근로시간2':
-                page_url = ''
+# def data_index_btn():
+#     data_list = ['연령별 임금 및 근로시간', '연령별 임금 및 근로시간2']
+#     clicked_buttons = [False] * len(data_list)  # 각 버튼의 클릭 여부를 저장하는 리스트
+#
+#     for i, data in enumerate(data_list):
+#         clicked_buttons[i] = st.button(f'{data}')
+#
+#     # 각 버튼의 클릭 여부를 확인하여 이벤트 처리
+#     for i, data in enumerate(data_list):
+#         if clicked_buttons[i]:
+#             # 페이지 이동을 위한 URL 설정
+#             if data == '연령별 임금 및 근로시간':
+#                 page_url = 'pages/seoul_main.py'
+#                 st.switch_page(page_url) ### page 이동 ###
+#
+#             elif data == '연령별 임금 및 근로시간2':
+#                 page_url = ''
 
 
 
@@ -83,21 +93,26 @@ def main():
     ### loading 시 main page로 이동 ###
     pages()
 
-
+    ### custtom font 등록하기 ###
     fontRegistered()
-    fontNames = [f.name for f in fm.fontManager.ttflist]
-    default_font_index = fontNames.index("Malgun Gothic") if "Malgun Gothic" in fontNames else 0
-    #### session state에 값이 없으면 default_font_index로 설정하고, 값이 잇으면 session state에서 fontNames에서 선택한 font의 index를 찾아서 표시함
-    fontname = st.selectbox("폰트 선택", fontNames, index=fontNames.index(st.session_state.font) if st.session_state.font!='' else default_font_index)
+    # fontNames = [f.name for f in fm.fontManager.ttflist]
+    # default_font_index = fontNames.index("Malgun Gothic") if "Malgun Gothic" in fontNames else 0
+    # #### session state에 값이 없으면 default_font_index로 설정하고, 값이 잇으면 session state에서 fontNames에서 선택한 font의 index를 찾아서 표시함
+    # fontname = st.selectbox("폰트 선택", fontNames, index=fontNames.index(st.session_state.font) if st.session_state.font!='' else default_font_index)
+    #
+    # ### font 이름이 변경 되었을 때 감지 ###
+    # if st.session_state.font != fontname:
+    #     if "font" in st.session_state:
+    #         st.session_state.font = fontname
 
-    ### font 이름이 변경 되었을 때 감지 ###
-    if st.session_state.font != fontname:
-        if "font" in st.session_state:
-            st.session_state.font = fontname
+    # 초기 폰트를 Malgun Gothic으로 설정
+    initial_font = "Malgun Gothic"
 
+    # 폰트 이름 고정
+    st.session_state.font = initial_font
 
     main_text()
-    data_index_btn()
+    # data_index_btn()
 
 
 
---
+++ pages/jonro_food_graph.py
@@ -0,0 +1,193 @@
+import streamlit as st
+
+import folium
+from folium.plugins import MarkerCluster
+import geopandas as gpd
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import os
+import json
+from streamlit_folium import folium_static
+
+
+
+### map csv ###
+if 'df_map' not in st.session_state:
+    df_map = './data/map.csv'
+    df_map = pd.read_csv(df_map, encoding='utf-8-sig')
+    st.session_state.df_map = df_map
+
+### geo json ###
+
+if 'geo_data_json' not in st.session_state:
+    # JSON 파일 읽기
+    json_file_path = './data/geo_seoul.json'
+    with open(json_file_path, encoding='utf-8-sig') as file:
+        data = json.load(file)
+    st.session_state.geo_data_json = data
+
+
+
+
+def main_draw_map(df_fil,geo_data):
+    # 기본 지도 생성
+    m = folium.Map(location=[37.5665, 126.9780], zoom_start=12, width='100%', height='100%')
+
+    # 색상 지정
+    color_dict = {
+        '골목상권': 'blue',
+        '발달상권': 'green',
+        '전통시장': 'red',
+        '관광특구': 'purple',
+    }
+
+    # 상권 리스트
+    sales_top_5 = ['종각역', '대학로(혜화역)', '종로구청', '서대문역', '북촌(안국역)']
+    korean_food_top_5 = ['성곡미술관', '종로5가역', '동묘앞역(동묘)', '광장시장(광장전통시장)', '종로4가']
+    hof_drink_food_top_5 = ['광화문역', '삼청동', '동대문역 1번', '세검정초등학교', '이북5도청사']
+    western_food_top_5 = ['종각역', '세종마을음식문화거리(금천교시장)', '서촌(경복궁역)', '이화사거리', '통인시장']
+    chaina_food_top_5 = ['북촌(안국역)', '종로·청계 관광특구', '종로3가역', '동대문역']
+
+    # MarkerCluster 추가
+    marker_clusters = {}
+    for key in color_dict.keys():
+        marker_clusters[key] = MarkerCluster(name=key).add_to(m)
+
+    # 각 데이터를 마커로 추가
+    def add_markers(data):
+        for i, row in data.iterrows():
+            color = color_dict.get(row['상권_구분_코드_명'], 'gray')
+            cluster = marker_clusters.get(row['상권_구분_코드_명'], None)
+
+            # 마커 아이콘 및 색상 설정
+            icon_html = ''
+            popup_html = f"""
+            <div style="font-family: Arial; font-size: 14px;">
+                <span style="font-size: 18px; color: darkblue;"><b>{row['상권_코드_명']}</b></span><br>
+                {row['상권_구분_코드_명']}<br>
+                행정동 코드: {row['행정동']}<br>
+                직장인수 평균 : {row['직장인수_평균']:,.0f} 명 <br>
+                상주인구 평균 : {row['상주인구_평균']:,.0f} 명 <br>
+                최다업종 : {row['최다업종']} <br>
+                개업률 평균 : {row['개업률_평균']:,.0f} % <br>
+                폐업률 평균 : {row['폐업률_평균']:,.0f} % <br>
+                월매출 평균 : {row['월매출_평균']:,.0f} 원
+            </div>
+            """
+
+            # top5_list에 있는 상권_코드_명은 별 모양
+            if row['상권_코드_명'] in sales_top_5:
+                icon_html = f"""
+                <div style="color: yellow; font-size: 32px;">
+                    <i class="fa-solid fa-trophy fa-spin"></i>
+                </div>
+                """
+
+            elif row['상권_코드_명'] in korean_food_top_5:
+                icon_html = f"""
+                <div style="color: white; font-size: 32px;">
+                         <i class="fa-solid fa-bowl-rice fa-spin"></i>
+                </div>
+                """
+
+            elif row['상권_코드_명'] in hof_drink_food_top_5:
+                icon_html = f"""
+                <div style="color: blue; font-size: 32px;">
+                         <i class="fa-solid fa-beer-mug-empty fa-spin"></i>
+                </div>
+                """
+
+            elif row['상권_코드_명'] in western_food_top_5:
+                icon_html = f"""
+                <div style="color: green; font-size: 32px;">
+                         <i class="fa-solid fa-pizza-slice fa-spin"></i>
+                </div>
+                """
+
+            elif row['상권_코드_명'] in chaina_food_top_5:
+                icon_html = f"""
+                <div style="color: red; font-size: 32px;">
+                         <i class="fa-solid fa-bowl-food fa-spin"></i>
+                </div>
+                """
+
+            else:
+                icon_html = f"""
+                <div style="color: {color}; font-size: 16px;">
+                    <i class="fa fa-info-circle"></i>
+                </div>
+                """
+
+            if cluster:
+                icon = folium.DivIcon(html=icon_html)
+                popup = folium.Popup(popup_html, max_width=300)
+                folium.Marker(
+                    location=[row['위도'], row['경도']],
+                    icon=icon,
+                    popup=popup
+                ).add_to(cluster)
+
+    # 마커 추가
+    add_markers(df_fil)
+
+    # 각 데이터의 영역 면적을 원으로 표시
+    def add_circles(data):
+        for i, row in data.iterrows():
+            if row['자치구명'] == '종로구':
+                color = color_dict.get(row['상권_구분_코드_명'], 'gray')
+                folium.Circle(
+                    location=[row['위도'], row['경도']],
+                    radius=row['영역_면적'] / 3000,  # 제곱 미터 단위
+                    color=color,
+                    fill=True,
+                    fill_color=color,
+                    fill_opacity=0.1
+                ).add_to(m)
+
+    # 원 추가
+    add_circles(df_fil)
+
+    # 자치구 경계 데이터 읽어오기 (geojson 파일 경로 지정)
+    # geojson_path = f'../data/geo_seoul.json'
+    # geo_data = gpd.read_file(geojson_path)
+
+    # 자치구 경계 추가
+    folium.GeoJson(
+        geo_data,
+        name='자치구 경계',
+        style_function=lambda feature: {
+            'fillColor': 'lightblue' if feature['properties']['name'] == '종로구' else 'orange',
+            'color': 'black',
+            'weight': 1,
+            'fillOpacity': 0.3 if feature['properties']['name'] == '종로구' else 0.1,
+        }
+    ).add_to(m)
+
+    # 지도 크기 조절
+    m.get_root().html.add_child(folium.Element('<style> #map { height: 1200px; width: 1200px; } </style>'))
+
+
+    return m
+
+
+
+def main():
+
+    # 페이지 제목 설정
+    st.title('📣서울시 상권 지도 시각화')
+
+
+    df_map = st.session_state.df_map
+    geo_data_json = st.session_state.geo_data_json
+
+    m = main_draw_map(df_map,geo_data_json)
+
+    folium_static(m)
+
+
+
+if __name__ == "__main__":
+    main()
